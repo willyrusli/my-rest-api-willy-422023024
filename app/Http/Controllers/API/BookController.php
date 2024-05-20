0bf402b8d2906712d9cc160ed006ee1031836e08
willyrusli <willy.422023024@civitas.ukrida.ac.id>
2024-05-20 21:18:11 +0700

    api book initial

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the item.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::get();
+    }
+    /**
+     * Store a newly created item in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $book = new Book;
+            $book->fill($request->validated())->save();
+
+            return $book;
+
+        } catch(\Exception $exception) {
+            throw new HttpException(400, "Invalid data - {$exception->getMessage}");
+        }
+  }
+
+
+    /**
+     * Display the specified item.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::findOrfail($id);
+
+        return $book;
+    }
+
+    /**
+     * Update the specified item in storage.
+     * 
+     * @param \Illuminate\Http\Request #request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if (!$id) {
+            throw new HttpException(400, "Invalid id");
+        }
+
+        try{
+            $book = Book::find($id);
+            $book->fill($request->validated())->save();
+
+            return $book;
+
+        } catch(\Exception $exception) {
+            throw new HttpException(400, "Invalid data - {$exception->getMessage}");
+        }
+    }
+
+    /**
+     * Remove the specified item from storage.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::findOrfaill($id);
+        $book->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
+
+class Book extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+    protected $table = 'books' ;
+    protected $fillable = [
+        'title',
+        'auther',
+        'publisher',
+        'publication_year',
+        'cover',
+        'description',
+        'price',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by',
+    ];
+
+    public function data_adder(){
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
+}
